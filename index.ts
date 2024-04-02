0538a030db295357118e2781a1fbf935f0d71634
j2999 <162644620+j2999@users.noreply.github.com>
2024-04-01 22:30:05 -0400

    Fix exports

--- index.ts
+++ index.ts
@@ -3,9 +3,6 @@ import { ICacheStrategy } from './src/cache/interfaces/ICacheStrategy.js';
 import { NodeCacheStrategy } from './src/cache/NodeCacheStrategy.js';
 import { MongoCredentials, MONGO_CONNECTION_TYPE, MongoCredentialsDTO } from './src/config/db/credentials/MongoCredentials.js';
 import { BitcoinNetwork } from './src/config/enums/BitcoinNetwork.js';
-import { IConfig } from './src/config/interfaces/IConfig.js';
-import { ConfigBase } from './src/config/ConfigBase.js';
-import { ConfigManager } from './src/config/ConfigLoader.js';
 import { DataAccessErrorType } from './src/errors/enums/DataAccessErrorType.js';
 import { DataAccessError } from './src/errors/DataAccessError.js';
 import { DebugLevel } from './src/logger/enums/DebugLevel.js';
@@ -18,7 +15,10 @@ import { TypeConverter } from './src/utils/TypeConverter.js';
 import { UtilsConfigurations } from './src/utils/UtilsConfigurations.js';
 
 export { CacheStrategy, ICacheStrategy, NodeCacheStrategy } 
-export { MongoCredentials, MONGO_CONNECTION_TYPE, MongoCredentialsDTO, BitcoinNetwork, IConfig, ConfigBase, ConfigManager } 
+export { MongoCredentials, MONGO_CONNECTION_TYPE, MongoCredentialsDTO, BitcoinNetwork } 
 export { DataAccessErrorType, DataAccessError } 
 export { DebugLevel, ILogger, Logger } 
 export { BitcoinHelper, Globals, TickerHelper, TypeConverter, UtilsConfigurations } 
+export * from './src/config/ConfigBase.js'
+export * from './src/config/interfaces/IConfig.js';
+export * from './src/config/ConfigLoader.js';
